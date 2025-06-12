a4f5b9aa4a672927255458e96303f91b12a004ea
atara <atara8478@gmail.com>
2025-06-12 14:47:39 +0300

    3

---
+++ sofazone/src/components/Login/Login.lazy.tsx
@@ -0,0 +1,13 @@
+// import React, { lazy, Suspense } from 'react';
+
+import Login from "./Login";
+
+// const LazyLogin = lazy(() => import('./Login'));
+
+// const Login = (props: JSX.IntrinsicAttributes & { children?: React.ReactNode; }) => (
+//   <Suspense fallback={null}>
+//     <LazyLogin {...props} />
+//   </Suspense>
+// );
+
+ export default Login;
---
+++ sofazone/src/components/Login/Login.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './Login.scss';
+
+interface LoginProps {}
+
+const Login: FC<LoginProps> = () => (
+  <div className="Login">
+    Login Component
+  </div>
+);
+
+export default Login;
